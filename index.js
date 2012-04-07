8ca7c7647570a9e3fa060fd2ee5b65cde65b2934
Eric <eric@wineslingr.com>
2012-04-07 13:55:21 -0700

    updated index with some Mongode code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 dbs = require('./config/databases')
+mongode = require('mongode');
 
 // Usage notes: https://github.com/philippkueng/node-neo4j
 
@@ -12,3 +13,22 @@ dbs.neo4j.InsertNode({
   // Output node id.
   console.log(node.id);
 });
+
+
+coffeeRankerDb = mongode.connect('mongo://66.150.174.182:27017/CoffeeRanker');
+
+exports.getStoreLocation = function (companyId, locationId, response) {
+  	var storeLocationCollection = coffeeRankerDb.collection('StoreLocation');
+	response.writeHead(200, { 'Content-Type': 'application/json' });  
+	storeLocationCollection.findOne({$and: [{"CompanyId" : companyId}, 
+								{"LocationId" : locationId}]}, function(err, object) {
+		if(object != null)
+		{
+			response.write(JSON.stringify(object));
+		}
+		else
+		{
+    		response.end();
+		}
+    });
+}
